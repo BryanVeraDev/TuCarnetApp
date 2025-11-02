96f740296f9daa23b729a1f13db348a4de3daecd
Omar <omardavidjm@ufps.edu.co>
2025-11-02 03:15:35 -0500

    fix: correct logout listenter

--- app/src/main/java/com/example/tucarnetapp/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/tucarnetapp/ui/home/fragment/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tucarnetapp.ui.home.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.tucarnetapp.R
+import com.example.tucarnetapp.ui.home.HomeScreenActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +25,8 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var logoutButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,9 +39,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        logoutButton = view.findViewById(R.id.btnLogout)
+
+        logoutButton.setOnClickListener {
+            val intent = Intent(requireContext(), HomeScreenActivity::class.java)
+            startActivity(intent)
+
+            requireActivity().finish()
+        }
+
+        return view
     }
+
 
     companion object {
         /**
