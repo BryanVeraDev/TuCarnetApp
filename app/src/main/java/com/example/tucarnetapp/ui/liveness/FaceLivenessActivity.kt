de307c6c7fe7f8b992b692df2d0f145838adbf99
Omar <omardavidjm@ufps.edu.co>
2025-11-18 06:55:10 -0500

    feat: add Rekognition flow for check liveness in login

---
+++ app/src/main/java/com/example/tucarnetapp/ui/liveness/FaceLivenessActivity.kt
@@ -0,0 +1,143 @@
+package com.example.tucarnetapp.ui.liveness
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import aws.smithy.kotlin.runtime.time.Instant
+import com.amplifyframework.auth.AWSCredentialsProvider
+import com.amplifyframework.auth.AWSTemporaryCredentials
+import com.amplifyframework.auth.AuthException
+import com.amplifyframework.core.Consumer
+import com.amplifyframework.ui.liveness.model.FaceLivenessDetectionException
+import com.amplifyframework.ui.liveness.ui.FaceLivenessDetector
+import com.amplifyframework.ui.liveness.ui.LivenessColorScheme
+import com.example.tucarnetapp.data.repository.LivenessRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class FaceLivenessActivity : ComponentActivity() {
+
+    private val repository = LivenessRepository()
+
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            val allGranted = results.values.all { it }
+            if (allGranted) startLivenessFlow()
+            else {
+                Toast.makeText(this, "Se requieren cámara y audio", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        val allGranted = requiredPermissions.all { perm ->
+            ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (allGranted) startLivenessFlow()
+        else permissionLauncher.launch(requiredPermissions)
+    }
+
+    private fun startLivenessFlow() {
+
+        lifecycleScope.launch {
+
+            val response = try {
+                withContext(Dispatchers.IO) {
+                    repository.createLivenessSession()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@FaceLivenessActivity, "Error creando sesión", Toast.LENGTH_LONG).show()
+                finish()
+                return@launch
+            }
+
+            Log.d("DEBUG_LIVENESS", "Respuesta backend completa: $response")
+
+            val sessionId = response.sessionId
+            val creds = response.credentials
+
+            if (sessionId.isNullOrBlank()) {
+                Toast.makeText(this@FaceLivenessActivity, "Backend retornó sessionId vacío", Toast.LENGTH_LONG).show()
+                finish()
+                return@launch
+            }
+
+            setContent {
+
+                MaterialTheme(colorScheme = LivenessColorScheme.default()) {
+                    Box(modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)){
+                        FaceLivenessDetector(
+                            sessionId = sessionId,
+                            region = "us-east-1",
+
+                            credentialsProvider =
+                                object : AWSCredentialsProvider<AWSTemporaryCredentials> {
+
+                                    override fun fetchAWSCredentials(
+                                        onSuccess: Consumer<AWSTemporaryCredentials>,
+                                        onError: Consumer<AuthException>
+                                    ) {
+
+                                        try {
+                                            val awsCreds = AWSTemporaryCredentials(
+                                                creds.accessKeyId,
+                                                creds.secretAccessKey,
+                                                creds.sessionToken,
+                                                Instant.fromIso8601(creds.expiration)
+                                            )
+
+                                            onSuccess.accept(awsCreds)
+
+                                        } catch (e: Exception) {
+                                            onError.accept(AuthException("Credenciales inválidas", "Hola"))
+                                        }
+                                    }
+                                },
+
+                            onComplete = {
+                                Log.i("Liveness", "Verificación completada")
+                                Toast.makeText(this@FaceLivenessActivity, "Verificación exitosa", Toast.LENGTH_LONG).show()
+                                finish()
+                            },
+
+                            onError = { error: FaceLivenessDetectionException ->
+                                Log.e("Liveness", "Error en verificación")
+                                Toast.makeText(this@FaceLivenessActivity, "Error: ${error.message}", Toast.LENGTH_LONG).show()
+                                finish()
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
